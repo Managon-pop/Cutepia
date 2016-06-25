5eaff8220a456774f3cbf69b0cfa3b341bf1d42d
Manago-n! <github123pop@gmail.com>
2016-06-25 12:48:20 +0900

    Add NPCs..?

--- src/Managon/cutepia/cutepiaentity/npc/CutepiaVillager.php
+++ src/Managon/cutepia/cutepiaentity/npc/CutepiaVillager.php
@@ -2,9 +2,10 @@
 
 namespace Managon\cutepia\cutepiaentity\npc;
 
+use Managon\cutepia\Cutepia;
 use Managon\cutepia\CutepiaEntity;
 
-abstract class CutepiaNPC extends CutepiaEntity
+class CutepiaVillager extends CutepiaNPC
 {
-	
+
 }
